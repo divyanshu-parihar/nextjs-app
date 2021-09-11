55ea78d3ad7a21f0d187ee4466082af245fc4af3
Divyanshu Parihar <divyanshu1447@gmail.com>
2021-09-11 12:16:40 +0530

    used head component for head tag in html

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const about = () => {
+    return (
+        <div>
+    <Head>
+        <title>WebDev NewZ</title>
+        <meta name="Keywords" content="web development"></meta>
+      </Head>
+            <h1>About Page !</h1>
+        </div>
+    )
+}
+
+export default about
